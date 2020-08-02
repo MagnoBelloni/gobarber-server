43186fa5fdc3960dfded29f6a20c59781052c8f9
Magno Belloni <bellonidesouza6@gmail.com>
2020-08-01 22:36:26 -0300

    Routes data validation.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { json, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
@@ -17,6 +18,8 @@ app.use(cors());
 app.use(json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     if (err instanceof AppError) {
